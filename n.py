5f077e4dc70a882d4c4f21bb0fa0b0bffd1c564c
Oybek <123@mail.com>
2024-08-07 20:44:14 +0500

    first

---
+++ n.py
@@ -0,0 +1,29 @@
+# 1
+# import math
+
+
+# a = 200
+# b = 350
+# c = (a ** 2 + b ** 2) ** 0.5
+# print(f"the answer gipotenuza is {c}")
+
+
+# 2
+# import math as M 
+# radius = 7
+# ploshad = M.pi *  radius * radius
+# с = 10
+# perimetr = 2 * M.pi * radius
+# d = 15
+# dlina_okrujnosti = M.pi * d
+
+
+# print(" The площадь of the given circle is: ", ploshad) 
+# print(" The периметр of the given circle is: ", perimetr) 
+# print(" The длин окружности of the given circle is: ", dlina_okrujnosti) 
+
+# 3
+import math
+x = math.pi / 4
+function = math.sin(x) + math.cos(x)
+print(function)
